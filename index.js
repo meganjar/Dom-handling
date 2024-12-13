e7e4646381a56efbd1321aef1fe3c6b0fa1ce56e
Jesus <jjcorrales76@gmail.com>
2024-12-13 16:45:18 -0700

    changes

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ for (let i = 0; i < menuLinks.length; i++) {
     linkElement.setAttribute("href", menuLinks[i].href)
     linkElement.textContent = (menuLinks[i].text);
     topMenuEl.appendChild(linkElement);
-    console.log(linkElement);
+    //console.log(linkElement);
 }
 
 //PART3: Creating the submenu
@@ -85,7 +85,25 @@ subMenuEl.style.position = "absolute";
 subMenuEl.style.top = "0";
 
 //Adding Menu Interaction
+//Select and cache the all of the <a> elements inside of 
+// topMenuEl in a variable named topMenuLinks.
 let topMenuLinks = topMenuEl.querySelectorAll("a");
+
+/*2. Attach a delegated 'click' event listener to topMenuEl.
+The first line of code of the event listener function should call the event object's preventDefault() method.
+The second line of code of the function should immediately return if the element clicked was not an <a> element.
+Log the content of the <a> to verify the handler is working.*/
+
+topMenuEl.addEventListener("click", function(event) {
+    event.preventDefault();
+    if(!event.target.matches("a")) {
+      return;
+    }
+    const clickedLink = event.target;
+    topMenuLinks.forEach((link) => {
+      link.classList.remove("active");
+    });
+})  
 
 
 
