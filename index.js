7aa24a028e466d6999a715fd839a7b0baec8ca33
Jesus <jjcorrales76@gmail.com>
2024-12-13 16:12:02 -0700

    changes

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 // Menu data structure
 var menuLinks = [
     { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+    {
+        text: 'catalog', href: '#', subLinks: [
+            { text: 'all', href: '/catalog/all' },
+            { text: 'top selling', href: '/catalog/top' },
+            { text: 'search', href: '/catalog/search' },
+        ]
+    },
+    {
+        text: 'orders', href: '#', subLinks: [
+            { text: 'new', href: '/orders/new' },
+            { text: 'pending', href: '/orders/pending' },
+            { text: 'history', href: '/orders/history' },
+        ]
+    },
+    {
+        text: 'account', href: '#', subLinks: [
+            { text: 'profile', href: '/account/profile' },
+            { text: 'sign out', href: '/account/signout' },
+        ]
+    },
+];
 
 
 // 1. Select and cache the <main> element in a variable named mainEl.
@@ -16,7 +33,7 @@ mainEl.style.backgroundColor = 'var(--main-bg)';
 
 //3. Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; 
 // pick whichever one that you think works best in this situation.
-mainEl.innerHTML = `<h1>DOM Manipulation</h1>`; 
+mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
 
 //4. Add a class of flex-ctr to mainEl.
 //Hint: Use the Element.classList API.
@@ -31,7 +48,7 @@ let topMenuEl = document.querySelector("#top-menu")
 topMenuEl.style.height = "100%";
 
 //3.
-topMenuEl.style.backgroundColor = 'var(--sub-menu-bg)'
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
 
 //4. 
 topMenuEl.classList.add('flex-around');
@@ -41,11 +58,34 @@ topMenuEl.classList.add('flex-around');
 //2.
 //3.
 //4.
-for (let i = 0; i < menuLinks.length; i++){
-let linkElement = document.createElement("a");
-linkElement.setAttribute("href", menuLinks[i].href)
-linkElement.textContent = (menuLinks[i].text);
-topMenuEl.appendChild(linkElement);
-console.log(linkElement);
+for (let i = 0; i < menuLinks.length; i++) {
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href", menuLinks[i].href)
+    linkElement.textContent = (menuLinks[i].text);
+    topMenuEl.appendChild(linkElement);
+    console.log(linkElement);
 }
+
+//PART3: Creating the submenu
+//1. Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl.
+let subMenuEl = document.getElementById("sub-menu");
+//console.log("subMenuEl Element: ", subMenuEl )
+//2.
+subMenuEl.style.height = "100%";
+
+//3.
+topMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+
+//4.
+subMenuEl.classList.add('flex-around');
+
+//setting position properties
+//1.
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+//Adding Menu Interaction
+let topMenuLinks = topMenuEl.querySelectorAll("a");
+
+
 
